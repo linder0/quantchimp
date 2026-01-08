75ac3a504287f3505a8a17a0e946339a11b4a21f
linder0 <xuelinda7@gmail.com>
2026-01-08 00:45:29 -0500

    Initial Commit

---
+++ quantchimp/quantchimpApp.swift
@@ -0,0 +1,17 @@
+//
+//  quantchimpApp.swift
+//  quantchimp
+//
+//  Created by Linda Xue on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct quantchimpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quantchimpTests/quantchimpTests.swift
@@ -0,0 +1,17 @@
+//
+//  quantchimpTests.swift
+//  quantchimpTests
+//
+//  Created by Linda Xue on 1/8/26.
+//
+
+import Testing
+@testable import quantchimp
+
+struct quantchimpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
